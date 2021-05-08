4d75a05a8fe9a94ffe32b9b88e945626c8f98c38
Sarah Rivera <sarahkrivera@gmail.com>
2021-05-08 14:49:00 -0400

    removed functions from global scope

--- scripts.js
+++ scripts.js
@@ -1,19 +1,27 @@
-let store = 0;
-function increment() {
-  store = store + 1;
-  counter.innerHTML = store;
-}
-function doAlert() {
-  increment();
-  alert("you are the 100th visitor. you win! click to claim your prize");
-}
 function onWindowLoad() {
+  let store = 0;
+
+  function increment(counter) {
+    store = store + 1;
+    counter.innerHTML = store;
+  }
+
+  function doAlert(counter) {
+    increment(counter);
+    alert("you are the 100th visitor. you win! click to claim your prize");
+  }
+
+  function click() {
+    doAlert(counter);
+  }
+
   // bind click event
-  document.querySelector("#clickbait").addEventListener("click", doAlert);
+  document.querySelector("#clickbait").addEventListener("click", click);
 
   const counter = document.querySelector("#counter");
   counter.innerHTML = store;
 
   document.querySelector("#dabutton").addEventListener("click", increment);
 }
+window.onload = null;
 window.onload = onWindowLoad;
